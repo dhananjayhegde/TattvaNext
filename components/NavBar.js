411b5f16ee23f44a97a72639eb25ccfb4703e2c1
Dhananjay Hegde <dananjay123@gmail.com>
2022-03-27 22:03:04 +0530

    Add logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,10 +8,10 @@ export default function NavBar({}){
                 <FontAwesomeIcon className='mr-2 my-2 text-2xl' icon={['fab', 'whatsapp']}/>
                 <a href='https://wa.me/918099664362'>Contact Me</a>
             </div>
-            <nav className="z-10 flex justify-between w-4/5 bg-slate-800 text-gray-200 font-bold px-4 py-4 rounded-lg m-auto relative top-10">
+            <nav className="z-10 flex justify-between w-4/5 bg-[#292826] text-gray-200 font-bold px-4 py-4 rounded-lg m-auto relative top-10">
                 <div className="logo-container relative">
-                    <figure className="absolute -left-8 -top-8 w-20 h-20 overflow-hidden rounded-full border border-4 border-slate-100 shadow-lg shadow-grey-500/40">
-                        <img src="/logo.png" alt="Logo" className="object-cover"/>
+                    <figure className="absolute -left-8 -top-8 p-2 w-20 h-20 overflow-hidden rounded-full border border-4 border-slate-100 shadow-lg bg-[#292826] shadow-grey-500/40">
+                        <img src="/tattva_logo__man-only-orange-black-bg.png" alt="Logo" className="object-cover"/>
                     </figure>
                 </div>
                 <ul className='flex'>
