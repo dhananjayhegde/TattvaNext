3e70c3557737f7dddf34ae4db91b4e8f8078e51d
Dhananjay Hegde <dananjay123@gmail.com>
2022-03-28 13:04:14 +0530

    Adjust featured programs sections for desktop

--- pages/index.js
+++ pages/index.js
@@ -69,20 +69,23 @@ export default function Home() {
       </section>
       
       
-      <section className='flex flex-col md:flex-row justify-start mt-10 min-h-min bg-slate-100'>
-        <div className='flex flex-col items-center mx-8'>
+      <section className='flex flex-col md:flex-row justify-start mt-10 md:px-32 pb-8 min-h-min bg-slate-100'>
+        <div className='flex flex-col items-center mx-8 md:mx-32'>
           {/* Section Heading */}
-          <h2 className='text-3xl font-bold text-gray-600 my-8 pb-4 border-b-2 border-slate-200/0.5'>Programs Offered</h2>
+          <h2 className='text-4xl md:text-6xl font-bold text-gray-600 my-8 pb-4 border-b-2 border-slate-200/0.5'>Programs Offered</h2>
           
-          {/* Cards */}
-        
-          <div className='card-2 flex min-h-24 w-full rounded-lg my-4 pb-4 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
+          {/* Cards */}        
+          
+          <div className='card-2 flex flex-col md:flex-row min-h-24 w-full rounded-lg my-4 py-4 md:py-8 px-4 md:px-8 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
             <div className='flex flex-col justify-center items-center basis-1/4'>
               <img src="/images/surya-kriya.png" alt="Surya Kriya" className="object-cover object-center w-full h-full drop-shadow-2"/>
             </div>
-            <div className='flex flex-col justify-center basis-3/4 mx-1 p-2 mx-2'>
-              <h2 className='text-2xl font-bold text-gray-600'>Surya Kriya</h2>
-              <ul className='text-xs text-gray-500 mt-2 list-disc list-inside'>
+            <div className='flex flex-col justify-start basis-3/4 mx-1 p-2 mx-2'>
+              <h2 className='text-3xl md:text-4xl font-bold text-gray-700'>Surya Kriya</h2>
+              <p className='text-sm md:text-base italic text-gray-500 my-2'>
+                Surya Kriya is a potent 21-step yogic practice of tremendous antiquity, designed as a holistic process for health and inner wellbeing.
+              </p>
+              <ul className='text-sm md:text-base text-gray-600 mt-2 list-disc list-inside'>
                 <li>Develops mental clarity & focus</li>
                 <li>Remedies weak constitution</li>
                 <li>Boosts vigor & viatlity</li>
@@ -90,13 +93,16 @@ export default function Home() {
             </div>
           </div>
 
-          <div className='card-2 flex min-h-24 w-full rounded-lg my-4 pb-4 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
+          <div className='card-2 flex flex-col md:flex-row min-h-24 w-full rounded-lg my-4 py-4 md:py-8 px-4 md:px-8 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
             <div className='flex flex-col justify-center items-center basis-1/4 mx-2'>
               <img src="/images/angamardana.png" alt="Angamardana" className="object-cover object-center w-full h-full drop-shadow-2"/>
             </div>
-            <div className='flex flex-col justify-center basis-3/4 mx-1 p-2'>
-              <h2 className='text-2xl font-bold text-gray-600'>Angamardana</h2>
-              <ul className='text-xs text-gray-500 mt-2 list-disc list-inside'>
+            <div className='flex flex-col justify-start basis-3/4 mx-1 p-2'>
+              <h2 className='text-3xl md:text-4xl font-bold text-gray-700'>Angamardana</h2>
+              <p className='text-sm md:text-base italic text-gray-500 my-2'>
+                Surya Kriya is a potent 21-step yogic practice of tremendous antiquity, designed as a holistic process for health and inner wellbeing.
+              </p>
+              <ul className='text-sm md:text-base text-gray-600 mt-2 list-disc list-inside'>
                 <li>Strengths the spine</li>
                 <li>Builds physical strength</li>
                 <li>Prepares the body for Hatha Yoga</li>
@@ -104,13 +110,16 @@ export default function Home() {
             </div>
           </div>
 
-          <div className='card-2 flex min-h-24 w-full rounded-lg my-4 pb-4 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
+          <div className='card-2 flex flex-col md:flex-row  min-h-24 w-full rounded-lg my-4 py-4 md:py-8 px-4 md:px-8 overflow-hidden bg-white shadow-lg shadow-slate-100/0.5'>
             <div className='flex flex-col justify-center items-center basis-1/4 mx-2'>
               <img src="/images/yogasanas.png" alt="Yogasanas" className="object-cover object-center w-full h-full drop-shadow-2"/>
             </div>
-            <div className='flex flex-col justify-center basis-3/4 mx-1 p-2'>
-              <h2 className='text-2xl font-bold text-gray-600'>Yogasanas</h2>
-              <ul className='text-xs text-gray-500 mt-2 list-disc list-inside'>
+            <div className='flex flex-col justify-start basis-3/4 mx-1 p-2'>
+              <h2 className='text-3xl md:text-4xl font-bold text-gray-700'>Yogasanas</h2>
+              <p className='text-sm md:text-base italic text-gray-500 my-2'>
+                Surya Kriya is a potent 21-step yogic practice of tremendous antiquity, designed as a holistic process for health and inner wellbeing.
+              </p>
+              <ul className='text-sm md:text-base text-gray-600 mt-2 list-disc list-inside'>
                 <li>Relieves from chronic health issues</li>
                 <li>Stabilizes body & mind</li>
                 <li>Deceleratin of aging process</li>
