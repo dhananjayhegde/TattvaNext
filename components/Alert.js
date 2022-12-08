d8063ad2105eb6d45b4af03e82ab53d799c3ed00
Dhananjay Hegde <dananjay123@gmail.com>
2022-12-08 08:09:26 +0530

    Adjust styles of  Alert

--- components/Alert.js
+++ components/Alert.js
@@ -2,19 +2,12 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Alert({message, type}){
-    let className = "py-2 px-8 rounded-lg tracking-wide text-center";
-    if(type === 'error'){
-        className += " bg-red-300"
-    }else if(type === 'warning'){
-        className += " bg-amber-300"
-    }else if(type === 'info'){
-        className += " bg-blue-300"
-    }else {
-        className += " bg-teal-300"
-    }
+    let className = `alert ${type}`;
+    let icon = type === "ok" ? "circle-check" : "exclamation";
     return (
         <div className={className}>
-            <p>{message}</p>
+            <FontAwesomeIcon icon={icon} className='mr-4 text-gray-600'/>
+            <span>{message}</span>
         </div>
     )
 }
